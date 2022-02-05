d6da1179ebba077e140359ecb6e8154c487ee344
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2022-02-05 19:33:09 +0530

    added recursive solution

--- 33_Search_in_Rotated_Sorted_Array.cpp
+++ 33_Search_in_Rotated_Sorted_Array.cpp
@@ -1,11 +1,18 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
+// iterative solution
 // https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/1702729/C%2B%2B-find-pivot-with-binary-search-or-O(log-n)
+
+// recursive solution
+// https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/1746787/Recursion-Solution-or-C%2B%2B-or-with-comments-%3A)
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
+// iterative solution
+// ===========================================================
 int pivot(vector<int> &nums)
 {
     int start = 0;
@@ -64,12 +71,65 @@ int search(vector<int> &nums, int target)
     else
         return binarySearch(nums, target, pivotEle + 1, nums.size() - 1);
 }
+// =========================================================
+
+// Recursive Solution
+// ****************************************************
+int helper(vector<int> &arr, int target, int start, int end)
+{
+    if (start > end)
+        return -1;
+
+    int mid = start + (end - start) / 2;
+
+    if (arr[mid] == target)
+        return mid;
+
+    if (arr[start] <= arr[mid])
+    {
+        // left sorted part
+        if (target >= arr[start] && target <= arr[mid])
+        {
+            // if target lies in this sorted range, search in this only, discard right part, by end = mid - 1
+            end = mid - 1;
+        }
+        else
+        {
+            // means, it does not lies here, so discard this part, by, start = mid + 1
+            start = mid + 1;
+        }
+    }
+    else
+    {
+        // arr[start] > arr[mid]
+        if (target >= arr[mid] && target <= arr[end])
+        {
+            // target is in this range, so discard left part (start = mid + 1), now search in this
+            start = mid + 1;
+        }
+        else
+        {
+            // else, it's not in that range, so search in left part now
+            end = mid - 1;
+        }
+    }
+    // recursive call
+    return helper(arr, target, start, end);
+}
+
+int search2(vector<int> &nums, int target)
+{
+    // return what we are getting from helper function
+    return helper(nums, target, 0, nums.size() - 1);
+    // we made helper, so that, we can pass start and end after computing, in another recursion calls
+}
+// **************************************************
 
 int main()
 {
     vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
     int target = 0;
 
-    cout << search(nums, target) << '\n';
+    cout << search2(nums, target) << '\n';
     return 0;
 }
