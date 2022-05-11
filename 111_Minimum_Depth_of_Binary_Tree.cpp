775879f15bcb68582e97c93eb3e4334aed1548e8
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2022-05-11 21:06:10 +0530

    added links

--- 111_Minimum_Depth_of_Binary_Tree.cpp
+++ 111_Minimum_Depth_of_Binary_Tree.cpp
@@ -1,3 +1,7 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/2029434/Simple-and-Clean-Code-C%2B%2B
+
 #include <iostream>
 using namespace std;
 
@@ -10,7 +14,6 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
 int minDepth(TreeNode *root)
 {
     if (root == NULL)
@@ -19,7 +22,7 @@ int minDepth(TreeNode *root)
     int left = minDepth(root->left);
     int right = minDepth(root->right);
 
-    if (left == 0 || right == 0)
+    if (left == 0 || right == 0) // if any of both is zero, return non zero value
         return 1 + max(left, right);
 
     return 1 + min(left, right);
