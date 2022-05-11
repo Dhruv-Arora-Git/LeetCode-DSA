e3ce9a55d71f93e191b98e7e9073079b62b37c19
Dhruv-Arora-Git <dhruvarora595@gmail.com>
2022-05-11 21:02:07 +0530

    no comments needed

---
+++ 111_Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int minDepth(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int left = minDepth(root->left);
+    int right = minDepth(root->right);
+
+    if (left == 0 || right == 0)
+        return 1 + max(left, right);
+
+    return 1 + min(left, right);
+}
+
+int main()
+{
+    // left skewed tree
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->left->left = new TreeNode(4);
+    root->left->left->left->left = new TreeNode(5);
+    root->left->left->left->left->left = new TreeNode(6);
+
+    cout << minDepth(root) << "\n";
+
+    return 0;
+}
